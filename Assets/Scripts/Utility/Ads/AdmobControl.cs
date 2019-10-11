0fb2ede44b97ebd87c5ab2bb965e1e1036391cf8
caoanhit <caohuynhanh97@gmail.com>
2019-10-11 23:45:50 +0700

    Add Admob Sdk

---
+++ Assets/Scripts/Utility/Ads/AdmobControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdmobControl : MonoBehaviour
+{
+    private RewardedAd rewardedAd;
+    const string rewardedAdId = "ca-app-pub-3940256099942544/5224354917";
+    void Start()
+    {
+        MobileAds.Initialize(initStatus => { });
+        this.rewardedAd = new RewardedAd(rewardedAdId);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ShowAd()
+    {
+        if (this.rewardedAd.IsLoaded())
+        {
+            this.rewardedAd.Show();
+        }
+    }
+}
